89ca00112af7418ceec23cb19fe2c6996750b1ae
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-09-10 18:54:44 +0200

    :tada: Update build

--- dist/styles.d.ts
+++ dist/styles.d.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export declare const TimelineDiv: import("styled-components").StyledComponent<"div", any, {
     timelineBg?: string | undefined;
 }, never>;
@@ -18,24 +17,3 @@ export declare const TimelineItemHeaderH4: import("styled-components").StyledCom
     titleColor?: string | undefined;
     titleBg?: string | undefined;
 }, never>;
-=======
-export declare const TimelineDiv: import("styled-components").StyledComponent<"div", any, {
-    timelineBg: string;
-}, never>;
-export declare const TimelineItemDiv: import("styled-components").StyledComponent<"div", any, {
-    bgColor: string;
-}, never>;
-export declare const TimelineItemBodyDiv: import("styled-components").StyledComponent<"div", any, {
-    bgColor: string;
-    borderRadius: string;
-    bodyTextColor: string;
-}, never>;
-export declare const TimelineItemHeaderDiv: import("styled-components").StyledComponent<"div", any, {}, never>;
-export declare const TimelineItemHeaderP: import("styled-components").StyledComponent<"p", any, {
-    dateColor: string;
-}, never>;
-export declare const TimelineItemHeaderH4: import("styled-components").StyledComponent<"h4", any, {
-    titleColor: string;
-    titleBg: string;
-}, never>;
->>>>>>> c445d9e9feb65b0a453e338e00ea8bc400301d97
